3d41bfc196e6addafbf5e11392bc2cd147a41554
CFdefense <CFdefence@gmail.com>
2026-01-04 13:36:34 -0500

    added thread code

--- Chapter16_Fearless_Concurrency/threads/src/main.rs
+++ Chapter16_Fearless_Concurrency/threads/src/main.rs
@@ -1,3 +1,66 @@
+use std::thread;
+use std::time::Duration;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn creates_thread() {
+        // Spawn a new thread
+        thread::spawn(|| {
+            // Print messages from the spawned thread
+            for i in 1..10 {
+                println!("hi number {i} from the spawned thread!");
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        // Print messages from the main thread
+        for i in 1..5 {
+            println!("hi number {i} from the main thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    }
+
+    #[test]
+    fn joins_thread() {
+        // Spawn a new thread
+        // Handle is a JoinHandle<T>
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {i} from the spawned thread!");
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+    
+        // Print messages from the main thread
+        for i in 1..5 {
+            println!("hi number {i} from the main thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    
+        // Join the thread
+        // This will block the main thread until the spawned thread finishes
+        handle.join().unwrap();
+    }
+
+    #[test]
+    fn moves_ownership() {
+        // Create a new vector
+        let v = vec![1, 2, 3];
+
+        // Spawn a new thread
+        // Move the vector into the closure so that the spawned thread owns it
+        let handle = thread::spawn(move || {
+            println!("Here's a vector: {v:?}");
+        });
+
+        // Join the thread
+        // This will block the main thread until the spawned thread finishes
+        handle.join().unwrap();
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
